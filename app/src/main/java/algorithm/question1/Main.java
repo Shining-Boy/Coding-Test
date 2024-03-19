66768c97d9b856772457bb9c9ec4dd462c4ee6e9
hsbae <hellospring89@gmail.com>
2024-03-20 00:24:36 +0900

    문제1 : 입출력부 구현

--- app/src/main/java/algorithm/question1/Main.java
+++ app/src/main/java/algorithm/question1/Main.java
@@ -1,7 +1,33 @@
 package algorithm.question1;
 
-    public class Main {
-        public static void main(String[] args) {
-        System.out.println("question1");
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+// 주어진 직선좌표의 교점을 찾아 별 만들기
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String inputpath = System.getProperty("user.dir") + "\\app\\src\\main\\java\\algorithm\\question1\\";
+        BufferedReader br = new BufferedReader(new FileReader(inputpath + "input.txt"));
+
+        int numberOfLine = Integer.parseInt(br.readLine());
+        int inputData[][] = new int [numberOfLine][]; 
+        for(int i=0; i<numberOfLine; i++) {
+            String line = br.readLine();
+            if (line==null) break;  
+
+            String[] values = line.split(" ");
+            int[] lineData = new int[values.length];
+            for(int j=0; j<values.length; j++){
+                lineData[j] = Integer.parseInt(values[j]);
+            }
+            inputData[i] = lineData;
+        }
+        br.close();
+
+        Solution solutionInstance = new Solution();
+        for(String eachLine : solutionInstance.solution(inputData)){
+            System.out.println(eachLine);
+        }
     }
 }
---
+++ app/src/main/java/algorithm/question1/Solution.java
@@ -0,0 +1,17 @@
+package algorithm.question1;
+
+public class Solution {
+    private static class Point {
+        public final long x, y;
+        
+        private Point(long x, long y){
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public String[] solution(int[][] line){
+        String[] result = new String[10];
+        return result;
+    }
+}
