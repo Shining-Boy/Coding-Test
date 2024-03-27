6567fab873334a3ccf5194025e59339951137181
hsbae <hellospring89@gmail.com>
2024-03-27 23:17:35 +0900

    문제4: 행렬의곱셈 - 풀이 완료

--- app/src/main/java/algorithm/question4/Solution.java
+++ app/src/main/java/algorithm/question4/Solution.java
@@ -2,8 +2,17 @@ package algorithm.question4;
 
 public class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2){
-        int[][] arr = new int[0][0];
+        int[][] result = new int[arr1.length][arr2[0].length];
+        for(int i=0; i<arr1.length; i++){
+            for(int j=0; j<arr2[0].length; j++){
+                int newArrValue = 0;
+                for(int m=0; m<arr1[i].length; m++){
+                    newArrValue = newArrValue + arr1[i][m]*arr2[m][j];
+                }
+                result[i][j] = newArrValue;
+            }
+        }
 
-        return arr;
+        return result;
     }
 }
