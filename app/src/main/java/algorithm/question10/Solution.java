dca3517581cc2b313aacd12197c54d156e4871ad
hsbae <hellospring89@gmail.com>
2024-04-10 12:15:09 +0900

    문제 10: 이진 변환 반복 - 풀이완료

--- app/src/main/java/algorithm/question10/Solution.java
+++ app/src/main/java/algorithm/question10/Solution.java
@@ -1,9 +1,33 @@
 package algorithm.question10;
 
 public class Solution {
-    public int[] solution(String s){
-        int[] result = new int[2];
+    private int convert(StringBuilder builder){
 
+        String s = builder.toString();
+        builder.setLength(0);
+        int numOfZero = 0;
+        for(char c : s.toCharArray()){
+            if(c == '0'){
+                numOfZero++;
+            }
+        }
+
+        builder.append(Integer.toString(s.length() - numOfZero, 2));
+
+        return numOfZero;
+    }
+
+    public int[] solution(String s){
+        int[] result = new int[]{0, 0};
+
+        StringBuilder builder = new StringBuilder(s);
+
+        while(!builder.toString().equals("1")){
+            int numOfZero = convert(builder);
+            result[0]++;
+            result[1] = result[1] + numOfZero;
+        }
+        
         return result;
     }
 }
