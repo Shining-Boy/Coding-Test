22cd1c2988a862fe579ce2afc725469d803f2b0e
hsbae <hellospring89@gmail.com>
2024-04-11 12:39:27 +0900

    문제 11: 문자열 내 p와 y의 개수 - 풀이완료

--- app/src/main/java/algorithm/question11/Solution.java
+++ app/src/main/java/algorithm/question11/Solution.java
@@ -2,7 +2,11 @@ package algorithm.question11;
 
 public class Solution {
     public Boolean solution(String s){
-        
-        return true;
+        String lowerStr = s.toLowerCase();
+
+        int numberOfP = s.length() - lowerStr.replace("p", "").length();
+        int numberOfS = s.length() - lowerStr.replace("y", "").length();
+
+        return numberOfP==numberOfS ? true : false;
     }
 }
