8fa6b77f0d265a0d27c14b5677d7c5022a4620f3
hsbae <hellospring89@gmail.com>
2024-04-12 23:40:51 +0900

    문제13: 정규표현식으로 매칭되는 문자열 찾기 - 풀이완료

--- app/src/main/java/algorithm/question13/Solution.java
+++ app/src/main/java/algorithm/question13/Solution.java
@@ -2,8 +2,8 @@ package algorithm.question13;
 
 public class Solution {
     public boolean solution(String s){
-        
+        boolean matches = s.matches("[0-9]{4}|[0-9]{6}");
 
-        return true;
+        return matches;
     }
 }
