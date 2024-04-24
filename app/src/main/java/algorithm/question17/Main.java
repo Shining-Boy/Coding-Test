7e200b6f5a6078cd413ca64b22446a85b298e6e9
hsbae <hellospring89@gmail.com>
2024-04-24 23:40:24 +0900

    문제17: 모음 사전 - 풀이 완료

---
+++ app/src/main/java/algorithm/question17/Main.java
@@ -0,0 +1,20 @@
+package algorithm.question17;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        String inputpath = System.getProperty("user.dir") + "\\app\\src\\main\\java\\algorithm\\question17\\";
+        BufferedReader br = new BufferedReader(new FileReader(inputpath + "input.txt"));
+
+        String inputData = br.readLine();
+        br.close();
+
+        Solution sol = new Solution();
+        int result = sol.solution(inputData);
+        System.out.println(result);
+    }
+}
---
+++ app/src/main/java/algorithm/question17/Solution.java
@@ -0,0 +1,8 @@
+package algorithm.question17;
+
+public class Solution {
+    public int solution(String s){
+
+        return 0;
+    }
+}
