be3ae5138d99a99be4fdbea1b9ca4971660158b6
hsbae <hellospring89@gmail.com>
2024-05-02 16:42:34 +0900

    문제17: 모음 사전 - 풀이 완료

--- app/src/main/java/algorithm/question17/Solution.java
+++ app/src/main/java/algorithm/question17/Solution.java
@@ -1,8 +1,26 @@
 package algorithm.question17;
 
-public class Solution {
-    public int solution(String s){
+import java.util.ArrayList;
+import java.util.List;
 
-        return 0;
+public class Solution {
+    private static final char[] CHARS = new char[]{'A', 'E', 'I', 'O', 'U'};
+
+    private List<String> generate(String word) {
+        List<String> words = new ArrayList<>();
+        words.add(word);
+
+        if(word.length()==5)
+            return words;
+
+        for(char c : CHARS){
+            words.addAll(generate(word + c));
+        }
+
+        return words;
+    }
+
+    public int solution(String s){
+        return generate("").indexOf(s);
     }
 }
