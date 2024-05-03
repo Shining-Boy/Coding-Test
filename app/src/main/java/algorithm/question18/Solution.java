e8b931b33061f9f31c6a7670a6d5d842fa0f77d7
hsbae <hellospring89@gmail.com>
2024-05-03 18:11:18 +0900

    문제18: 모의고사 - 풀이 완료

--- app/src/main/java/algorithm/question18/Solution.java
+++ app/src/main/java/algorithm/question18/Solution.java
@@ -1,14 +1,47 @@
 package algorithm.question18;
 
-public class Solution {
-    private int getPicked(int person, int problem){
+import java.util.ArrayList;
+import java.util.List;
 
-        return 0;
+public class Solution {
+    private static final int[][] RULES = {
+        {1, 2, 3, 4, 5},
+        {2, 1, 2, 3, 2, 4, 2, 5},
+        {3, 3, 1, 1, 2, 2, 4, 4, 5, 5}
+    };
+
+    private int getPicked(int person, int problem){
+        int[] rule = RULES[person];
+        int index = problem % rule.length;
+
+        return rule[index];
     }
 
     public int[] solution(int[] answers) {
-        int[] corrects = new int[3];
+        int[] corrects = new int[]{0, 0, 0};
 
-        return corrects;
+        int maxCorrect = 0;
+        for(int i=0; i<3; i++){
+            for(int j=0; j<answers.length; j++){
+                if(answers[j] == getPicked(i, j)){
+                    corrects[i]++;
+                }
+            }
+            maxCorrect = maxCorrect < corrects[i] ? corrects[i] : maxCorrect;
+        }
+
+        List<Integer> result = new ArrayList<>();
+        for(int i=0; i<3; i++){
+            if(corrects[i] == maxCorrect){
+                result.add(i+1);
+            }
+        }
+
+        int[] result2 = new int[result.size()];
+        for(int i=0; i<result.size(); i++){
+            result2[i] = result.get(i);
+        }
+
+        return result2;
     }
 }
