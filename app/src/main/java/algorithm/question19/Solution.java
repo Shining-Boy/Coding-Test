53677e0d025ea6b0a981e4b28aa0b801c3229696
hsbae <hellospring89@gmail.com>
2024-05-09 00:12:05 +0900

    문제19: 카펫 - 풀이 완료

--- app/src/main/java/algorithm/question19/Solution.java
+++ app/src/main/java/algorithm/question19/Solution.java
@@ -2,8 +2,15 @@ package algorithm.question19;
 
 public class Solution {
     public int[] solution(int brown, int yellow){
-        int[] result = new int[2];
 
-        return result;
+        for(int width=3; width<=2499; width++){
+            for(int height=3; height<=width; height++){
+                if((width+height-2)*2==brown && width*height-brown==yellow){
+                    return new int[]{width, height};
+                }
+            }
+        }
+
+        return null;
     }
 }
