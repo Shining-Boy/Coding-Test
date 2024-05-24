aebea2219470d3d389f46ac1899f1574b190e6cd
hsbae <hellospring89@gmail.com>
2024-05-24 23:37:08 +0900

    문제21: 소수 찾기 - 풀이완료 v1

--- app/src/main/java/algorithm/question21/Solution.java
+++ app/src/main/java/algorithm/question21/Solution.java
@@ -1,8 +1,40 @@
 package algorithm.question21;
 
-public class Solution {
-    public int solution(String numbers){
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-        return 0;
+public class Solution {
+    private boolean isPrime(int n){
+        if(n <= 1) return false;
+
+        for(int i=2; i<n; i++){
+            if(n % i == 0) return false;
+        }
+
+        return true;
+    }
+
+    private Set<Integer> getPrimes(int acc, List<Integer> numbers){
+        Set<Integer> primes = new HashSet<>();
+        if(isPrime(acc)) primes.add(acc);
+
+        for(int i=0; i<numbers.size(); i++){
+            List<Integer> newNumbers = new ArrayList<>(numbers);
+            newNumbers.remove(i);
+            primes.addAll(getPrimes(acc*10 + numbers.get(i).intValue(), newNumbers));
+        }
+
+        return primes;
+    }
+
+    public int solution(String numbers){
+        List<Integer> convertedNumbers = new ArrayList<>();
+        for(char c : numbers.toCharArray()){
+            convertedNumbers.add(c - '0');
+        }
+
+        return getPrimes(0, convertedNumbers).size();
     }
 }
